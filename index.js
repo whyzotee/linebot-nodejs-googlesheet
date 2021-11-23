3b78863e2acdececb2b29fcc0392dc980b8134a9
Z0TEExt <zenlektomyum@gmail.com>
2021-11-23 12:40:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.get("/", (req, res) => {
 app.post("/webhook", function(req, res) {
   res.send("HTTP POST request sent to the webhook URL!")
   // If the user sends a message to your bot, send a reply message
-  if (req.body.events[0].message === "สีเหลือง") message == "Yellow!"
-  else if(req.body.events[0].message === "มะม่วง") message == "Mango!";
+  if (req.body.events[0].message.text === "สีเหลือง") message == "Yellow!"
+  else if(req.body.events[0].message.text === "มะม่วง") message == "Mango!";
   if (req.body.events[0].type === "message") {
     
     // Message data, must be stringified
