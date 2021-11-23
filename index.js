6b37f23b8c9e5e98845a5000a318dedb0c0489d2
Z0TEExt <zenlektomyum@gmail.com>
2021-11-23 12:03:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ app.get("/", (req, res) => {
 
 app.post("/webhook", function(req, res) {
   res.send("HTTP POST request sent to the webhook URL!")
-  // If the user sends a message to your bot, send a reply message
+  
   if (req.body.events[0].type === "message") {
-    // Message data, must be stringified
+    
     const dataString = JSON.stringify({
       replyToken: req.body.events[0].replyToken,
       messages: [
         {
-          "type": "text",
-          "text": "Hello, user"
+          "type": "สีเหลือง",
+          "text": "Yellow"
         },
         {
-          "type": "text",
-          "text": "May I help you?"
+          "type": "มะม่วง",
+          "text": "Mango"
         }
       ]
     })
@@ -38,7 +38,6 @@ app.post("/webhook", function(req, res) {
       "Authorization": "Bearer " + TOKEN
     }
 
-    // Options to pass into the request
     const webhookOptions = {
       "hostname": "api.line.me",
       "path": "/v2/bot/message/reply",
@@ -47,19 +46,16 @@ app.post("/webhook", function(req, res) {
       "body": dataString
     }
 
-    // Define request
     const request = https.request(webhookOptions, (res) => {
       res.on("data", (d) => {
         process.stdout.write(d)
       })
     })
 
-    // Handle error
     request.on("error", (err) => {
       console.error(err)
     })
 
-    // Send data
     request.write(dataString)
     request.end()
   }
